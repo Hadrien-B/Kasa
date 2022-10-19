df9ccc7a154344a078cfdbfaa5b75dcb17cca2b2
Hadrien Borie <borie.hadrien@gmail.com>
2022-10-19 19:40:23 +0200

    Bannière

--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
 
 const Home = () => {
-  return <div>Home</div>
+  return (
+    <div>
+      <section className="container banner__container">
+        <h1>Chez vous, partout et ailleurs</h1>
+      </section>
+    </div>
+  )
 }
 
 export default Home
