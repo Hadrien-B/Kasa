ea360d8aca8d10a51fae4066bc8f231cc33aa747
Hadrien Borie <borie.hadrien@gmail.com>
2022-10-20 16:52:23 +0200

    footer

--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import logo from '../../assets/logo_Footer.svg'
+import './footer.css'
 
 const Footer = () => {
-  return <div>Footer</div>
+  return (
+    <div className="footer">
+      <img src={logo} alt="logo Kasa" className="footer__logo" />
+      <p className="text__footer">© 2020 Kasa, ALl rigths reserved</p>
+    </div>
+  )
 }
 
 export default Footer
