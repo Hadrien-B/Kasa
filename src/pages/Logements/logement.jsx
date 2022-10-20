e4dadb62f7b79930b93c24f82f85edef63dac9bf
Hadrien Borie <borie.hadrien@gmail.com>
2022-10-20 12:39:04 +0200

    gallerie

--- src/pages/Logements/logement.jsx
+++ src/pages/Logements/logement.jsx
@@ -1,7 +1,36 @@
 import React from 'react'
+import './logement.css'
+import { data } from '../../API/data'
 
 const Logement = () => {
-  return <div>Logement</div>
+  return (
+    <section className="container gallery__container">
+      <article className="gallery__card">
+        <div className="gallery__card-image"></div>
+        <h3>Titre de la location</h3>
+      </article>
+      <article className="gallery__card">
+        <div className="gallery__card-image"></div>
+        <h3>Titre de la location</h3>
+      </article>
+      <article className="gallery__card">
+        <div className="gallery__card-image"></div>
+        <h3>Titre de la location</h3>
+      </article>
+      <article className="gallery__card">
+        <div className="gallery__card-image"></div>
+        <h3>Titre de la location</h3>
+      </article>
+      <article className="gallery__card">
+        <div className="gallery__card-image"></div>
+        <h3>Titre de la location</h3>
+      </article>
+      <article className="gallery__card">
+        <div className="gallery__card-image"></div>
+        <h3>Titre de la location</h3>
+      </article>
+    </section>
+  )
 }
 
 export default Logement
