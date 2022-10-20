e50d03a0cb08359566c1311d4d617e53d30986b1
Hadrien Borie <borie.hadrien@gmail.com>
2022-10-20 16:53:33 +0200

    gallerie 90%

--- src/pages/Logements/logement.jsx
+++ src/pages/Logements/logement.jsx
@@ -1,34 +1,24 @@
 import React from 'react'
 import './logement.css'
+import axios from 'axios'
+import { NavLink } from 'react-router-dom'
 import { data } from '../../API/data'
 
 const Logement = () => {
   return (
     <section className="container gallery__container">
-      <article className="gallery__card">
-        <div className="gallery__card-image"></div>
-        <h3>Titre de la location</h3>
-      </article>
-      <article className="gallery__card">
-        <div className="gallery__card-image"></div>
-        <h3>Titre de la location</h3>
-      </article>
-      <article className="gallery__card">
-        <div className="gallery__card-image"></div>
-        <h3>Titre de la location</h3>
-      </article>
-      <article className="gallery__card">
-        <div className="gallery__card-image"></div>
-        <h3>Titre de la location</h3>
-      </article>
-      <article className="gallery__card">
-        <div className="gallery__card-image"></div>
-        <h3>Titre de la location</h3>
-      </article>
-      <article className="gallery__card">
-        <div className="gallery__card-image"></div>
-        <h3>Titre de la location</h3>
-      </article>
+      {data.map(({ id, cover, title }) => {
+        return (
+          <NavLink to={`/logement/${data.id}`}>
+            <article key={id} className="gallery__card">
+              <div>
+                <img src={cover} alt={title} className="gallery__card-image" />
+              </div>
+              <h3>{title}</h3>
+            </article>
+          </NavLink>
+        )
+      })}
     </section>
   )
 }
