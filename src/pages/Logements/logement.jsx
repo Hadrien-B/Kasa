599b8739583a5b01cc3f6bfd87a0e0338583cda5
Hadrien Borie <borie.hadrien@gmail.com>
2022-10-20 18:40:16 +0200

    dégradé sur images

--- src/pages/Logements/logement.jsx
+++ src/pages/Logements/logement.jsx
@@ -11,9 +11,8 @@ const Logement = () => {
         return (
           <NavLink to={`/logement/${data.id}`}>
             <article key={id} className="gallery__card">
-              <div>
-                <img src={cover} alt={title} className="gallery__card-image" />
-              </div>
+              <div className="gradient__card"></div>
+              <img src={cover} alt={title} className="gallery__card-image" />
               <h3>{title}</h3>
             </article>
           </NavLink>
