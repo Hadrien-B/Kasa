e75ee91549675f32fd9332e7f8d64a4f32bf5a55
Hadrien Borie <borie.hadrien@gmail.com>
2022-10-21 16:55:42 +0200

    réorganisation

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './banner.css'
+
+const Banner = () => {
+  return (
+    <div>
+      <section className="container banner__container">
+        <h1 className="text__banner">Chez vous, partout et ailleurs</h1>
+      </section>
+    </div>
+  )
+}
+
+export default Banner
