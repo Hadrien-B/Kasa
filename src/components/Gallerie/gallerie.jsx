0a1b90cfd2bd82b6127007778a20e0801cdaea1d
Hadrien Borie <borie.hadrien@gmail.com>
2022-10-21 17:00:15 +0200

    ajout composant jsx header/banner

--- src/components/Gallerie/gallerie.jsx
+++ src/components/Gallerie/gallerie.jsx
@@ -3,22 +3,28 @@ import './gallerie.css'
 import axios from 'axios'
 import { NavLink } from 'react-router-dom'
 import { data } from '../../API/data'
+import Header from '../Header/Header'
+import Banner from '../Banner/Banner'
 
 const Gallerie = () => {
   return (
-    <section className="container gallery__container">
-      {data.map(({ id, cover, title }) => {
-        return (
-          <NavLink to={`/logement/${data.id}`}>
-            <article key={id} className="gallery__card">
-              <div className="gradient__card"></div>
-              <img src={cover} alt={title} className="gallery__card-image" />
-              <h3>{title}</h3>
-            </article>
-          </NavLink>
-        )
-      })}
-    </section>
+    <div>
+      <Header />
+      <Banner />
+      <section className="container gallery__container">
+        {data.map(({ id, cover, title }) => {
+          return (
+            <NavLink to={`/logement/${data.id}`}>
+              <article key={id} className="gallery__card">
+                <div className="gradient__card"></div>
+                <img src={cover} alt={title} className="gallery__card-image" />
+                <h3>{title}</h3>
+              </article>
+            </NavLink>
+          )
+        })}
+      </section>
+    </div>
   )
 }
 
