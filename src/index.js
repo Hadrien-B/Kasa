53f7bc4724fe66cd66d848948404a91f42cf2ff3
Hadrien Borie <borie.hadrien@gmail.com>
2022-10-21 16:26:37 +0200

    ajout des routes vers différentes pages

--- src/index.js
+++ src/index.js
@@ -4,8 +4,11 @@ import './index.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Header from './components/Header/Header';
 import Banner from './pages/Banner/banner';
-import Logement from './pages/Logements/logement';
+import Gallerie from './components/Gallerie/gallerie';
 import Footer from './components/Footer/Footer'
+import Logement from './pages/Logements/logement';
+import About from './pages/About/about';
+import Error from './pages/Error/error';
 
 const app = document.getElementById('root')
 const root = createRoot(app)
@@ -14,7 +17,12 @@ root.render(
     <BrowserRouter>
       <Header />
       <Banner />
-      <Logement />
+      <Routes>
+        <Route path='/' element={<Gallerie />} />
+        <Route path='/logement' element={<Logement />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/error' element={<Error />} />
+      </Routes>
       <Footer />
     </BrowserRouter>
   </React.StrictMode>
