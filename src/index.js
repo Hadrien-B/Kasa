c95d9a79ece63f957739267e456132317c4b2f58
Hadrien Borie <borie.hadrien@gmail.com>
2022-10-21 16:58:49 +0200

    réorganisation

--- src/index.js
+++ src/index.js
@@ -2,9 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Header from './components/Header/Header';
-import Banner from './pages/Banner/banner';
-import Gallerie from './components/Gallerie/gallerie';
+import Gallerie from './components/Gallerie/Gallerie';
 import Footer from './components/Footer/Footer'
 import Logement from './pages/Logements/logement';
 import About from './pages/About/about';
@@ -15,13 +13,11 @@ const root = createRoot(app)
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Header />
-      <Banner />
       <Routes>
         <Route path='/' element={<Gallerie />} />
-        <Route path='/logement' element={<Logement />} />
+        <Route path='/logement/:id' element={<Logement />} />
         <Route path='/about' element={<About />} />
-        <Route path='/error' element={<Error />} />
+        <Route path='/*' element={<Error />} />
       </Routes>
       <Footer />
     </BrowserRouter>
