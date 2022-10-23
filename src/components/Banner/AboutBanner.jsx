6627284e07e2f0ab86b1a7f49b54db5756f670c2
Hadrien Borie <borie.hadrien@gmail.com>
2022-10-23 17:28:29 +0200

    bannière à propos

---
+++ src/components/Banner/AboutBanner.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './banner.css'
+
+const AboutBanner = () => {
+  return (
+    <section className="container banner__container about__banner"></section>
+  )
+}
+
+export default AboutBanner
