10f69004e26fbcfb801a32382add69bdb22d2ecc
Hadrien Borie <borie.hadrien@gmail.com>
2022-10-23 17:27:28 +0200

    essai collapse

--- src/pages/About/about.jsx
+++ src/pages/About/about.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import './about.css'
 import Header from '../../components/Header/Header'
+import AboutBanner from '../../components/Banner/AboutBanner'
+import Collapse from '../../components/Collapse/Collapse'
 
 const About = () => {
   return (
     <div>
-      <Header />A propos
+      <Header />
+      <AboutBanner />
+      <Collapse />
     </div>
   )
 }
