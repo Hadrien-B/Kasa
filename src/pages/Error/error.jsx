812afdd8dc5a94e75522cbc5735f9ff0af5f4255
Hadrien Borie <borie.hadrien@gmail.com>
2022-10-24 17:26:52 +0200

    page 404

--- src/pages/Error/error.jsx
+++ src/pages/Error/error.jsx
@@ -1,7 +1,22 @@
 import React from 'react'
+import Header from '../../components/Header/Header'
+import './error.css'
 
 const Error = () => {
-  return <div>Error</div>
+  return (
+    <div>
+      <Header />
+      <main className="container container__error">
+        <div className="error">404</div>
+        <div className="message__error">
+          Oups ! La page que vous demandez n'existe pas
+        </div>
+        <div className="link__error">
+          <a href="/">Retourner sur la page d'accueil</a>
+        </div>
+      </main>
+    </div>
+  )
 }
 
 export default Error
