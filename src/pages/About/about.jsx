5d9a370c3e4626568f764d54d66aa71b78ba9e17
Hadrien Borie <borie.hadrien@gmail.com>
2022-10-27 18:00:43 +0200

    ajout de class pour une meilleur gestion du css

--- src/pages/About/about.jsx
+++ src/pages/About/about.jsx
@@ -7,7 +7,7 @@ const About = () => {
   return (
     <div>
       <AboutBanner />
-      <div>
+      <div className="container container__about">
         <Collapse
           title="Fiabilité"
           description="Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées  par nos équipes."
--- src/pages/Logements/logement.jsx
+++ src/pages/Logements/logement.jsx
@@ -10,39 +10,54 @@ import { data } from '../../API/data'
 export default function Logement() {
   const { id } = useParams()
   const apartment = data.find((item) => item.id === id)
-  const { title, location, rating, host, equipments, description, pictures } =
-    apartment
+  const { title, location, rating, host, description, pictures } = apartment
+  const listEquipment = (equipments) => {
+    return (
+      <ul className="apartment__equipments">
+        {equipments.map((equipement, index) => {
+          return (
+            <li key={index} className="list__equipments">
+              {equipement}
+            </li>
+          )
+        })}
+      </ul>
+    )
+  }
 
   return (
     <div className="container container__apartment">
       <Carousel pictures={pictures} className />
-      <section className="container description__appartement">
-        <h1 className="title__apartment">{title}</h1>
-        <div className="location__apartment">{location}</div>
-        <div className="tags">
-          {apartment.tags.map((tag, index) => (
-            <Tag tag={tag} key={index} />
-          ))}
+      <section className="container description__apartment">
+        <div className="info__location">
+          <h1 className="title__apartment">{title}</h1>
+          <div className="location__apartment">{location}</div>
+          <div className="tags">
+            {apartment.tags.map((tag, index) => (
+              <Tag tag={tag} key={index} />
+            ))}
+          </div>
         </div>
-
-        <div className="info__apartment">
+        <div className="info__host">
           <div className="host">
-            <div>{host.name}</div>
+            <div className="name__host">
+              <div className="name__host__firstName">
+                {host.name.split(' ', 1)}
+              </div>
+              <div className="name__host__lasName">
+                {host.name.split(' ').pop()}
+              </div>
+            </div>
             <img src={host.picture} className="img__host" alt={host.name} />
           </div>
           <Stars rating={parseInt(rating)} />
         </div>
       </section>
       <div className="container container__collapse">
-        <Collapse
-          title="Description"
-          description={description}
-          className="collapse__apartment"
-        />
+        <Collapse title="Description" description={description} />
         <Collapse
           title="Equipements"
-          description={equipments}
-          className="collapse__apartment"
+          description={listEquipment(apartment.equipments)}
         />
       </div>
     </div>
