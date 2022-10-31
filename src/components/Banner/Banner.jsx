ab3f98727e4230345fc3cf88d49cddd4bab6783a
Hadrien Borie <borie.hadrien@gmail.com>
2022-10-31 16:22:07 +0100

    version mobile ok

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -5,7 +5,9 @@ const Banner = () => {
   return (
     <div>
       <section className="container banner__container">
-        <h1 className="text__banner">Chez vous, partout et ailleurs</h1>
+        <div className="text__container">
+          <h1 className="text__banner">Chez vous, partout et ailleurs</h1>
+        </div>
       </section>
     </div>
   )
