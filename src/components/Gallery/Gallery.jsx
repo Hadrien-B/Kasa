7b843f5001d25b21e06623228feaa749513d1a2d
Hadrien Borie <borie.hadrien@gmail.com>
2022-11-01 16:09:43 +0100

    version mobile

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -11,13 +11,19 @@ const Gallerie = () => {
       <section className="container gallery__container">
         {data.map(({ id, cover, title }) => {
           return (
-            <article key={data.id} className="gallery__card">
-              <Link to={`/logement/${id}`}>
-                <div className="gradient__card"></div>
-                <img src={cover} alt={title} className="gallery__card-image" />
-                <h3>{title}</h3>
-              </Link>
-            </article>
+            <div className="container__card">
+              <article key={data.id} className="gallery__card">
+                <Link to={`/logement/${id}`}>
+                  <div className="gradient__card"></div>
+                  <img
+                    src={cover}
+                    alt={title}
+                    className="gallery__card-image"
+                  />
+                  <h3>{title}</h3>
+                </Link>
+              </article>
+            </div>
           )
         })}
       </section>
