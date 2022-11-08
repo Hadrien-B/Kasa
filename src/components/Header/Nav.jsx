9ee4bdf5df6741e5ecbed628060eaced166a1f97
Hadrien Borie <borie.hadrien@gmail.com>
2022-11-08 09:51:25 +0100

    focus sur le lien dans la barre de navigation

--- src/components/Header/Nav.jsx
+++ src/components/Header/Nav.jsx
@@ -2,20 +2,20 @@ import React from 'react'
 import { NavLink } from 'react-router-dom'
 import './header.css' //Importation du style
 
+//Composant 'Menu de navigation'
 const Nav = () => {
-  //Composant 'Menu de navigation
   return (
     <div>
       <ul className="navigation">
         <NavLink
           to="/" //Lien vers page d'accueil
-          className={({ isActive }) => (isActive ? 'active' : 'link')} //Fonction permettant de sytliser le lien
+          className={({ isActive }) => (!isActive ? 'active' : 'inactive')}
         >
           <li className="nav">Accueil</li>
         </NavLink>
         <NavLink
-          to="/about" //Lien vers page A propos
-          className={({ isActive }) => (isActive ? 'active' : 'link')}
+          to="about" //Lien vers page A propos
+          className={({ isActive }) => (isActive ? 'active' : 'inactive')} //Fonction permettant de sytliser le lien lorsque la page est active
         >
           <li className="nav">A Propos</li>
         </NavLink>
--- src/index.js
+++ src/index.js
@@ -18,9 +18,9 @@ root.render(
       <div className='main'>
       <Header />
         <Routes>
-          <Route path='/' element={<Gallery />} />  {/* Route vers la page accueil */}
-          <Route path='/logement/:id' element={<Logement />} /> {/* Route vers la page logement selon l'id sélectionnée */}
-          <Route path='/about' element={<About />} /> {/* Route vers la page A propos */}
+          <Route exact path='/' element={<Gallery />} />  {/* Route vers la page accueil */}
+          <Route exact path='/logement/:id' element={<Logement />} /> {/* Route vers la page logement selon l'id sélectionnée */}
+          <Route exact path='/about' element={<About />} /> {/* Route vers la page A propos */}
           <Route path='/*' element={<Error />} /> {/* Route vers la page 404 si le chemin demandé n'existe pas */}
         </Routes>
         </div>
