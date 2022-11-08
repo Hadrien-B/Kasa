8e2f4976dbd770873e509fd20aeae9a74b23fa10
Hadrien Borie <borie.hadrien@gmail.com>
2022-11-08 16:45:26 +0100

    composant Card

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { data } from '../../API/data' //Importation des données depuis le fichier data.js
+import './card.css' //Importation du style
+
+const Card = () => {
+  return (
+    <section className="container gallery__container">
+      <div className="container__card">
+        {data.map(({ id, cover, title }) => {
+          /*On récupère les données de l'API*/
+          return (
+            <article key={id} className="gallery__card">
+              {' '}
+              {/* l'id est utilisée comme clef unique*/}
+              {/*On créé un lien grace à Link vers la page logement en utilisation l'id*/}
+              <Link to={`/logement/${id}`}>
+                <div className="gradient__card"></div>
+                <img /*Gestion de l'image*/
+                  src={cover} /*On récupère l'image du logement*/
+                  alt={title}
+                  className="gallery__card-image"
+                />
+                {/*On récupère le titre du logement*/}
+                <h3>{title}</h3>
+              </Link>
+            </article>
+          )
+        })}
+      </div>
+    </section>
+  )
+}
+
+export default Card
--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -1,39 +1,15 @@
 import React from 'react'
 import './gallery.css' //Importation du style
-import { Link } from 'react-router-dom'
-import { data } from '../../API/data' //Importation des données depuis le fichier data.js
 import Banner from '../Banner/Banner' //Importation du composant Banner (Bannière)
+import Card from '../Card/Card' //Importation du composant Card (Vignette logement)
 
 const Gallerie = () => {
   //Composant Galerie
   return (
     <div>
-      <Banner />{' '}
-      {/*On 'wrappe' notre composant banner afin de l'intégrer à la page*/}
-      <section className="container gallery__container">
-        <div className="container__card">
-          {data.map(({ id, cover, title }) => {
-            /*On récupère les données de l'API*/
-            return (
-              <article key={id} className="gallery__card">
-                {' '}
-                {/* l'id est utilisée comme clef unique*/}
-                {/*On créé un lien grace à Link vers la page logement en utilisation l'id*/}
-                <Link to={`/logement/${id}`}>
-                  <div className="gradient__card"></div>
-                  <img /*Gestion de l'image*/
-                    src={cover} /*On récupère l'image du logement*/
-                    alt={title}
-                    className="gallery__card-image"
-                  />
-                  {/*On récupère le titre du logement*/}
-                  <h3>{title}</h3>
-                </Link>
-              </article>
-            )
-          })}
-        </div>
-      </section>
+      {/*On 'wrappe' nos composants banner et card afin de les intégrer à la page*/}
+      <Banner />
+      <Card />
     </div>
   )
 }
